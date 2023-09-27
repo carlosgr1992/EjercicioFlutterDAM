387b7c5a71f7ca725e5b47e138b56a116bbf6731
carlosgr1992 <carlos.gr1992@gmail.com>
2023-09-27 18:57:35 +0200

    creada ruta inicial al loginView

--- lib/MyApp.dart
+++ lib/MyApp.dart
@@ -5,9 +5,12 @@ import 'package:flutter/material.dart';
 class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+
+    return MaterialApp(
+      title: "MyApp",
+      //initialRoute: "/loginView",
+    );
+
   }
 
-  
 }
