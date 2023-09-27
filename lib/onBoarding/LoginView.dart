1fb3bf89f8a9d5bf0b6b417fe247062991870fb8
carlosgr1992 <carlos.gr1992@gmail.com>
2023-09-27 19:45:04 +0200

    mayor personalización del loginView(colores,centrados,hint)

--- lib/onBoarding/LoginView.dart
+++ lib/onBoarding/LoginView.dart
@@ -9,26 +9,44 @@ class LoginView extends StatelessWidget {
     _context = context;
 
     return Scaffold(
-      appBar: AppBar(title: Text("Login"), centerTitle: true, backgroundColor: Colors.amberAccent,),
+      appBar: AppBar(
+        title: Text("Login"),
+        centerTitle: true,
+        backgroundColor: Colors.amberAccent,
+      ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Text("Bienvenido al login",
-          style: TextStyle(
-            fontSize: 30,
-            fontWeight: FontWeight.bold,
-            color: Colors.black
-          )),
-          TextField(
-            decoration: InputDecoration(
-              focusedBorder: UnderlineInputBorder(
-                borderSide: BorderSide(color: Colors.amberAccent),
+          SizedBox(height: 25),
+          Text(
+            "Bienvenido al login",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+          ),
+          SizedBox(height: 120),  // Espacio entre el texto y las cajas
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 50.0),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: "Introduzca su usuario",
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.amberAccent),
+                ),
               ),
             ),
           ),
-          TextFormField(
-            decoration: InputDecoration(
-              focusedBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.amberAccent)
+          SizedBox(height: 50),  // Espacio entre las cajas
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 50.0),
+            child: TextFormField(
+              decoration: InputDecoration(
+                hintText: "Introduzca su contraseña",
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.amberAccent),
+                ),
               ),
             ),
           ),
