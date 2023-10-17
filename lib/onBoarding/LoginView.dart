656e03149cd00ee79095435bf885e471442fa2ec
carlosgr1992 <carlos.gr1992@gmail.com>
2023-10-17 20:00:25 +0200

    Implementada la misma logica del splash en el login para que el usuario tenga que introducir sus datos si o si

--- lib/onBoarding/LoginView.dart
+++ lib/onBoarding/LoginView.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -5,6 +6,8 @@ class LoginView extends StatelessWidget {
   late final BuildContext _context;
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
+
+  FirebaseFirestore db = FirebaseFirestore.instance;
 
   @override
   Widget build(BuildContext context) {
@@ -99,14 +102,18 @@ class LoginView extends StatelessWidget {
   void onClickAceptar() async {
     try {
       await FirebaseAuth.instance.signInWithEmailAndPassword(
-        email: emailController.text,
-        password: passwordController.text,
+          email: emailController.text,
+          password: passwordController.text,
       );
+
+          compruebaSesion();
+
       ScaffoldMessenger.of(_context).showSnackBar(SnackBar(
         content: Text("¡Login realizado con éxito!"),
         duration: Duration(seconds: 2),
       ));
-      Navigator.of(_context).popAndPushNamed('/splashView');
+
+
     } on FirebaseAuthException catch (e) {
       print('Código de la excepción: ${e.code}');
       if (e.code == 'INVALID_LOGIN_CREDENTIALS') {
@@ -131,5 +138,19 @@ class LoginView extends StatelessWidget {
 
   void onClickRegistrar() {
     Navigator.of(_context).pushNamed("/registerView");
+  }
+
+  void compruebaSesion() async {
+    if (FirebaseAuth.instance.currentUser != null) {
+      //Guardamos el uid del usuario conectado actualmente en la variable uid
+      String uid = FirebaseAuth.instance.currentUser!.uid;
+      //recibimos los datos del usuario, ponemos await para que espere a que acabe de cargar los datos del usuario
+      DocumentSnapshot<Map<String, dynamic>> datos = await db.collection("Usuarios").doc(uid).get();
+      if (datos.exists) {
+        Navigator.of(_context).popAndPushNamed("/homeView");
+      } else {
+        Navigator.of(_context).popAndPushNamed("/perfilDataView");
+      }
+    }
   }
 }
