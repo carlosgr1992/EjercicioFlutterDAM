7ba27fa2ef61ce9f6410e7e2c05105589694cb91
carlosgr1992 <carlos.gr1992@gmail.com>
2023-11-11 14:03:23 +0100

    Aplicada lógica de rutas en el MyApp según plataforma de acceso

--- lib/MyApp.dart
+++ lib/MyApp.dart
@@ -1,33 +1,56 @@
-
-
 import 'package:ejercicio_flutter_dam/Main/HomeView.dart';
+import 'package:ejercicio_flutter_dam/Main/HomeViewWeb.dart';
 import 'package:ejercicio_flutter_dam/Main/PostView.dart';
 import 'package:ejercicio_flutter_dam/onBoarding/LoginView.dart';
 import 'package:ejercicio_flutter_dam/onBoarding/PerfilDataView.dart';
 import 'package:ejercicio_flutter_dam/onBoarding/RegisterView.dart';
+import 'package:ejercicio_flutter_dam/singletone/PlatformAdmin.dart';
 import 'package:ejercicio_flutter_dam/splash/SplashApp.dart';
 import 'package:flutter/material.dart';
 
 import 'Main/PostCreateView.dart';
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-    return MaterialApp(
-      title: "MyApp",
-      routes: {
-        "/loginView": (context) => LoginView(),
-        "/registerView": (context) => RegisterView(),
-        "/splashView": (context) => SplashApp(),
-        "/homeView": (context) => HomeView(),
-        "/perfilDataView": (context) => PerfilDataView(),
-        "/postView": (context) => PostView(),
-        "/postCreateView": (context) => PostCreateView(),
-      },
-      initialRoute: "/loginView",
-    );
-
+    if (PlatformAdmin().isWeb || PlatformAdmin().isDesktop) {
+      return MaterialApp(
+        title: "MyApp",
+        routes: {
+          "/loginView": (context) => LoginView(),
+          "/registerView": (context) => RegisterView(),
+          "/splashView": (context) => SplashApp(),
+          "/homeView": (context) => HomeViewWeb(),
+          "/perfilDataView": (context) => PerfilDataView(),
+          "/postView": (context) => PostView(),
+          "/postCreateView": (context) => PostCreateView(),
+        },
+        initialRoute: "/loginView",
+      );
+    } else if (PlatformAdmin().isMobile || PlatformAdmin().isTablet) {
+      return MaterialApp(
+        title: "MyApp",
+        routes: {
+          "/loginView": (context) => LoginView(),
+          "/registerView": (context) => RegisterView(),
+          "/splashView": (context) => SplashApp(),
+          "/homeView": (context) => HomeView(),
+          "/perfilDataView": (context) => PerfilDataView(),
+          "/postView": (context) => PostView(),
+          "/postCreateView": (context) => PostCreateView(),
+        },
+        initialRoute: "/splashView",
+      );
+    } else {
+      return MaterialApp(
+        title: "MyApp",
+        home: Scaffold(
+          body: Center(
+            child: Text("Plataforma no reconocida, por favor, comuníquese con nosotros para solucionarlo",
+            ),
+          ),
+        ),
+      );
+    }
   }
-
 }
