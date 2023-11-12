af55a394cd4a345510a16883058a2a39f09f9800
carlosgr1992 <carlos.gr1992@gmail.com>
2023-11-12 19:56:44 +0100

    Implementado login por SMS para usuarios conectados desde movil

--- lib/onBoarding/LoginViewMobile.dart
+++ lib/onBoarding/LoginViewMobile.dart
@@ -1,18 +1,70 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class LoginViewMobile extends StatelessWidget {
-  late final BuildContext _context;
-  final TextEditingController tecNumTelefono = TextEditingController();
-  final TextEditingController tecVerificacion = TextEditingController();
 
-  FirebaseFirestore db = FirebaseFirestore.instance;
-
+class LoginViewMobile extends StatefulWidget{
   @override
-  Widget build(BuildContext context) {
-    _context = context;
+  State<LoginViewMobile> createState() => _LoginViewMobileState();
+}
 
+class _LoginViewMobileState extends State<LoginViewMobile> {
+
+  TextEditingController tecNumTelefono=TextEditingController();
+  TextEditingController tecNumVerificacion=TextEditingController();
+  String codigoVerificacion="";
+  bool blMostrarVerificacion=false;
+
+  void generaCodigoSMS() async{
+
+    String sTelefono=tecNumTelefono.text;
+
+    await FirebaseAuth.instance.verifyPhoneNumber(
+        phoneNumber: sTelefono,
+        verificationCompleted: verificacionCompletada,
+        verificationFailed: verificacionFallida,
+        codeSent: codigoEnviado,
+        codeAutoRetrievalTimeout: tiempoDeEsperaAcabado);
+  }
+
+  void verificaCodigo() async{
+    String smsCode = tecNumVerificacion.text;
+
+    PhoneAuthCredential credential = PhoneAuthProvider.credential(verificationId: codigoVerificacion, smsCode: smsCode);
+
+    await FirebaseAuth.instance.signInWithCredential(credential);
+
+    Navigator.of(context).popAndPushNamed("/homeView");
+  }
+
+  void verificacionCompletada(PhoneAuthCredential credencial) async{
+
+    await FirebaseAuth.instance.signInWithCredential(credencial);
+
+    Navigator.of(context).popAndPushNamed("/homeView");
+  }
+
+  void verificacionFallida(FirebaseAuthException excepcion){
+    if (excepcion.code == 'invalid-phone-number') {
+      print('The provided phone number is not valid.');
+    }
+  }
+
+  void codigoEnviado(String codigo, int? resendToken) async{
+
+    codigoVerificacion=codigo;
+
+    setState(() {
+      blMostrarVerificacion=true;
+    });
+
+
+  }
+
+  void tiempoDeEsperaAcabado(String verID){
+
+  }
+
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Login móvil"),
@@ -51,7 +103,7 @@ class LoginViewMobile extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 50.0),
               child: TextField(
-                controller: tecVerificacion,
+                controller: tecNumVerificacion,
                 decoration: InputDecoration(
                   fillColor: Color(0xFFFFE6A5),
                   filled: true,
@@ -70,17 +122,27 @@ class LoginViewMobile extends StatelessWidget {
                   height: 90,
                 ),
                 ElevatedButton(
-                  onPressed: onClickSolicitarSMS,
+                  onPressed: generaCodigoSMS,
                   style: ButtonStyle(
                     backgroundColor: MaterialStateProperty.all(Colors.amberAccent),
                   ),
                   child: Text(
-                    "Solicitar SMS",
+                    "Enviar SMS",
                     style: TextStyle(color: Colors.black),
                   ),
                 ),
                 SizedBox(
                   width: 50,
+                ),
+                ElevatedButton(
+                  onPressed: verificaCodigo,
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.amberAccent),
+                  ),
+                  child: Text(
+                    "Aceptar",
+                    style: TextStyle(color: Colors.black),
+                  ),
                 ),
               ],
             )
@@ -90,27 +152,4 @@ class LoginViewMobile extends StatelessWidget {
     );
   }
 
-  void onClickSolicitarSMS() {
-    _startPhoneNumberVerification();
-  }
-
-  Future<void> _startPhoneNumberVerification() async {
-    String phoneNumber = tecNumTelefono.text;
-    try {
-      await FirebaseAuth.instance.verifyPhoneNumber(
-        phoneNumber: phoneNumber,
-          verificationCompleted: (PhoneAuthCredential credential) {
-            FirebaseAuth.instance.signInWithCredential(credential);
-            Navigator.of(_context).popAndPushNamed("/homeView");
-          },
-        verificationFailed: (FirebaseAuthException e) {
-          print("Error de verificación: ${e.message}");
-        },
-        codeSent: (String verificationId, int? resendToken) {},
-        codeAutoRetrievalTimeout: (String verificationId) {},
-      );
-    } catch (e) {
-      print("Error al solicitar SMS: $e");
-    }
-  }
 }
